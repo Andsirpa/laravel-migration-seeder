4fca6aceda4c687b975c52b0ab591be6bdb4c82e
Andsirpa <andsirpa94@gmail.com>
2024-03-21 15:47:50 +0100

    add columns

--- database/migrations/2024_03_21_142011_create_trains_table.php
+++ database/migrations/2024_03_21_142011_create_trains_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      *
@@ -14,7 +13,18 @@ return new class extends Migration
     public function up()
     {
         Schema::create('trains', function (Blueprint $table) {
+            // assegno le colonne
             $table->id();
+            // $table->tipo di dato('nome_colonna', varchar)
+            $table->string('company', 50);
+            $table->string('from', 50);
+            $table->string('to', 50);
+            $table->dateTime('departure_time');
+            $table->dateTime('arrival_time');
+            $table->string('train_code', 15)->unique();
+            $table->tinyInteger('coaches_number')->nullable();
+            $table->boolean('on_time');
+            $table->boolean('cancelled');
             $table->timestamps();
         });
     }
