4d6a7bc7586bb67158c6579d87425c049f7cd519
Andsirpa <andsirpa94@gmail.com>
2024-03-26 22:49:36 +0100

    feat: create info using faker

--- app/Models/Train.php
+++ app/Models/Train.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Train extends Model
 {
     use HasFactory;
+
+    // uso il fillable per decidere quali
+    protected $fillable = [
+        "company",
+        "from",
+        "to",
+        "departure_time",
+        "arrival_time",
+        "train_code",
+        "coaches_number",
+
+    ];
 }
--- database/seeders/TrainSeeder.php
+++ database/seeders/TrainSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
 
 class TrainSeeder extends Seeder
 {
@@ -12,8 +13,32 @@ class TrainSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+
+    //  genero dati finti usando il faker
+    public function run(Faker $faker)
     {
-        //
+        for ($i = 0; $i < 100; $i++) {
+
+            $train = new Train;
+
+            $train->company = $faker->company();
+            $train->from = $faker->address();
+            $train->to = $faker->address();
+            $train->departure_time = $faker->date() . " " . $faker->time();
+            $train->arrival_time = $faker->date() . " " . $faker->time();
+
+            $train_code = "";
+
+            for ($y = 0; $y < 10; $y++) {
+                $train_code .= $faker->randomLetter();
+            }
+
+            $train->train_code = $train_code;
+            $train->coaches_number = $faker->numberBetween(5, 10);
+
+            $train->save();
+        }
     }
+
+
 }
